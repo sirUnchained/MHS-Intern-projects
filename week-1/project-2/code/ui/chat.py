06e210602bf8526273f72819c5fff9b1f0d262ee
sirunchained <bbehnam3831@gmail.com>
2026-07-03 22:39:59 +0330

    I added stream and changed the chat.py file

--- week-1/project-2/code/ui/chat.py
+++ week-1/project-2/code/ui/chat.py
@@ -1,28 +1,24 @@
 from langchain_core.runnables.config import RunnableConfig
+from langchain_core.messages import AIMessageChunk
+from langgraph.graph.state import CompiledStateGraph
 
 
-def process_user_message(prompt: str, thread_id: str, agent) -> str:
+async def process_user_message(prompt: str, thread_id: str, agent: CompiledStateGraph):
     """
-    Invoke the agent with the given prompt and return the final answer.
+    Invoke the agent with the given prompt and get the final answer with stream!
 
     Args:
         prompt: The user's message.
         thread_id: Conversation thread identifier (for memory isolation).
         agent: The compiled LangGraph agent.
-
-    Returns:
-        The agent's final text response.
     """
     config = RunnableConfig({"configurable": {"thread_id": thread_id}})
-    # token = context.current_thread_id.set(thread_id)
 
-    try:
-        response = agent.invoke(
-            {"messages": [{"role": "user", "content": prompt}]},
-            config=config,
-        )
-    finally:
-        pass
-        # context.current_thread_id.reset(token)
-
-    return response["messages"][-1].content
+    # We only need the message (chunk) for now
+    for chunk, _ in agent.stream(
+        {"messages": [{"role": "user", "content": prompt}]},
+        config=config,
+        stream_mode="messages",
+    ):
+        if isinstance(chunk, AIMessageChunk) and chunk.content:
+            yield chunk.content
